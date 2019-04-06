1d07f54154382ef18c907b67e5ae5e511ae2d044
Paul Millerd <paulmillerd@gmail.com>
2019-04-06 13:09:11 -0400

    Pinch to zoom; date taken

--- app/src/main/java/com/paulmillerd/photogalleryapp/fullscreenImage/FullscreenImageFragment.kt
+++ app/src/main/java/com/paulmillerd/photogalleryapp/fullscreenImage/FullscreenImageFragment.kt
@@ -1,15 +1,23 @@
 package com.paulmillerd.photogalleryapp.fullscreenImage
 
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import androidx.transition.TransitionManager
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.paulmillerd.photogalleryapp.R
 import com.paulmillerd.photogalleryapp.models.Photo
 import kotlinx.android.synthetic.main.fullscreen_image_layout.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class FullscreenImageFragment : Fragment() {
 
@@ -23,9 +31,18 @@ class FullscreenImageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        populateUi(arguments?.getSerializable(PHOTO) as Photo?)
 
-        val photo = arguments?.getSerializable(PHOTO) as Photo?
+        main_image_view.setOnClickListener {
+            val newVisibility = if (info_group.isVisible) GONE else VISIBLE
+            TransitionManager.beginDelayedTransition(info_group.parent as ViewGroup)
+            info_group.visibility = newVisibility
+        }
 
+        info_background.setOnClickListener { /* consume click */ }
+    }
+
+    private fun populateUi(photo: Photo?) {
         Glide.with(this)
             .load(photo?.bigUrl)
             .transition(DrawableTransitionOptions.withCrossFade())
@@ -34,6 +51,29 @@ class FullscreenImageFragment : Fragment() {
                     .load(photo?.smallUrl)
             )
             .into(main_image_view)
+
+        title.text = photo?.name
+
+        if (!TextUtils.isEmpty(photo?.description)) {
+            description.visibility = VISIBLE
+            description.text = HtmlCompat.fromHtml(photo?.description ?: "", HtmlCompat.FROM_HTML_MODE_COMPACT)
+        } else {
+            description.visibility = GONE
+        }
+
+        if (photo?.dateTaken != null && photo.username != null) {
+            taken_by.text = String.format(getString(R.string.taken_by_on), photo.username, formatDate(photo.dateTaken))
+            taken_by.visibility = VISIBLE
+        } else if (photo?.username != null) {
+            taken_by.text = String.format(getString(R.string.taken_by), photo.username)
+            taken_by.visibility = VISIBLE
+        } else {
+            taken_by.visibility = GONE
+        }
+    }
+
+    private fun formatDate(date: Date): String {
+        return SimpleDateFormat("MMMM dd, YYYY").format(date)
     }
 
 }
--- app/src/main/java/com/paulmillerd/photogalleryapp/modelConverters/PhotoFromApiConverter.kt
+++ app/src/main/java/com/paulmillerd/photogalleryapp/modelConverters/PhotoFromApiConverter.kt
@@ -3,18 +3,21 @@ package com.paulmillerd.photogalleryapp.modelConverters
 import com.paulmillerd.photogalleryapp.api.responseModels.PhotosItem
 import com.paulmillerd.photogalleryapp.models.ImageSize
 import com.paulmillerd.photogalleryapp.models.Photo
-import java.text.DateFormat
+import java.text.ParseException
+import java.text.SimpleDateFormat
 import java.util.*
 
 class PhotoFromApiConverter {
     companion object {
         fun convertApiModelToPhoto(apiModel: PhotosItem?): Photo {
-            var dateTaken: Date? = null
-            try {
-                dateTaken = DateFormat.getDateInstance().parse(apiModel?.takenAt)
-            } catch (e: Exception) {
-                e.printStackTrace()
-            }
+//            var dateTaken: Date? = null
+//            try {
+//                dateTaken = DateFormat.getDateInstance().parse(apiModel?.takenAt)
+//            } catch (e: Exception) {
+//                e.printStackTrace()
+//            }
+
+            val dateTaken = if (apiModel?.takenAt != null) parseIso8601Date(apiModel.takenAt) else null
 
             return Photo(
                 id = apiModel?.id,
@@ -27,6 +30,17 @@ class PhotoFromApiConverter {
                 smallUrl = apiModel?.images?.find { it?.size == ImageSize.SMALL.value }?.httpsUrl,
                 heightWidthRatio = (apiModel?.height?.toDouble() ?: 1.0) / (apiModel?.width?.toDouble() ?: 1.0)
             )
+        }
+
+        private fun parseIso8601Date(isoString: String): Date? {
+            val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:sszzzzz")
+            return try {
+                format.parse(isoString)
+            } catch (e: ParseException) {
+                e.printStackTrace()
+                null
+            }
+
         }
     }
 }
